c63167e42ff2a2eb3ec3e50b58f2050bf647215e
akkireddyanusha <akkireddyanusha@gmail.com>
2018-08-02 18:43:14 +0000

    Anushas most recent commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,36 +2,41 @@ require './config/environment'
 require './app/models/sample_model'
 
 class ApplicationController < Sinatra::Base
-configure do
-  set :public_folder, 'public'
-  set :views, 'app/views'
-end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
 
-get '/' do
-  return erb :index
-end
+  get '/' do
+    return erb :index
+  end
 
-get '/about_us' do
-  return erb :about_us
-end
+  get '/about_us' do
+   return erb :about_us
+  end
 
-get '/sexual_assault' do
-return erb :sexual_assault
-end
+  get '/sexual_assault' do
+    return erb :sexual_assault
+  end
 
-get '/self_harm' do
-return erb :self_harm
-end
+  get '/self_harm' do
+    return erb :self_harm
+  end
 
-get '/self_esteem' do
-return erb :self_esteem
-end
+  get '/self_esteem' do
+    return erb :self_esteem
+  end
 
-get '/forgotpassword' do
-  return erb :forgetpassword
-end
+  get '/forgotpassword' do
+    return erb :forgetpassword
+  end
 
-get '/self_esteem_advice'
-  return erb :advice_for_self_esteem
-end
+  get '/advice_for_self_esteem' do
+    return erb :advice_for_self_esteem
+  end
+
+get '/posts_for_self_esteem' do
+    return erb :posts_for_self_esteem
+  end
+
 end
